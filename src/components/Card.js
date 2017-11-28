1b67935d5d4d2b53ca5695ee26a2420f2b2d0249
Abbie Tuckner <abbietuckner@gmail.com>
2017-11-27 18:56:59 -0600

    Add styles and conditional for displaying matched cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,13 @@ import FontAwesome from 'react-fontawesome';
 class Card extends React.Component {
   render() {
     const props = this.props;
-    const cardStatus = props.selected ? 'Card-selected' : 'Card-unselected';
-    const icon = props.selected ?
+    let cardStatus = 'Card-unselected';
+
+    if (props.selected === true) { cardStatus = 'Card-selected' }
+
+    if (props.matched === true) { cardStatus = 'Card-matched' }
+
+    const icon = props.selected || props.matched ?
       <FontAwesome className={'Card-icon'} name={props.icon} size={'3x'} style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/> :
       <FontAwesome className={'Card-icon'} name={'question'} size={'3x'} style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}/>;
     return (
