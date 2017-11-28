68300788564886627ccd9e560239c10abb44a35d
Abbie Tuckner <abbietuckner@gmail.com>
2017-11-27 20:57:05 -0600

    Style updates

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Matching Game</h1>
-        </header>
+        <h1 className="App-title">Matching Game</h1>
         <Game/>
       </div>
     );
